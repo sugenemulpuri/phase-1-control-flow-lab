971c3c3a297115b60b973e7fb3a6f5212ae4e9c4
Sugene Mulpuri <sugenemulpuri9@gmail.com>
2024-02-20 17:10:29 -0600

    phase 1 control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,49 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number){
   // Write your code here!
+let answer
+if (number < 400) {
+  answer = "This one is on me!";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+else if (number >= 400 && number < 2000)
+{
+  answer = "That will be twenty bucks.";
 }
 
-function switchOnCharmFromTip(){
+else if (number >= 2000 && number < 2500)
+{
+  answer = "I will gladly take your thirty bucks.";
+}
+
+else if (number >= 2500)
+{
+  answer = "No can do.";
+}
+
+return answer;
+
+}
+
+function ternaryCheckCity(city){
   // Write your code here!
+
+  return city === "NYC" ? "Ok, sounds good." : "No go."
+
+}
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  let statement;
+  switch (tip) {
+    case "generous": 
+    statement = "Thank you so much."
+      break;
+    case "not as generous":
+      statement = "Thank you."
+      break;
+    default:
+      console.log("Bye.")
+      statement = "Bye."
+  }
+  return statement;
 }
